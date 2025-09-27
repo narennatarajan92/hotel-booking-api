5f00fc18054ce2805af372695a75407e13a09437
Naren <nareneee@outlook.com>
2025-09-27 19:40:05 +0530

    Refactor: BookingStepDefnitions

--- src/test/java/com/booking/stepdefinitions/BookingStepDefnitions.java
+++ src/test/java/com/booking/stepdefinitions/BookingStepDefnitions.java
@@ -10,22 +10,4 @@ import static org.hamcrest.Matchers.*;
 public class BookingStepDefnitions {
     private Response response;
 
-    @When("I want to read the messages")
-    public void iWantToReadTheMessages() {
-
-        response = given().log().all()
-                        .when()
-                        .get("https://automationintesting.online/api/message");
-    }
-
-    @Then("I should receive all existing messages")
-    public void iShouldReceiveAllExistingMessages() {
-        response.then().statusCode(200)
-                .body("messages.size()", greaterThan(0))
-                .body("messages.id", everyItem(notNullValue()))
-                .body("messages.name", everyItem(not(isEmptyOrNullString())))
-                .body("messages.subject", everyItem(not(isEmptyOrNullString())))
-                .body("messages.read", everyItem(notNullValue()))
-                .log().all();
-    }
 }
